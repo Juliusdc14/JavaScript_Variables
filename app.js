54caae17c92672336ea529a5ba776e9e4cacc2dc
juliusdc14 <juliusdc05@gmail.com>
2022-11-14 09:40:30 -0600

    completed java story

--- app.js
+++ app.js
@@ -4,7 +4,17 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "David";
+let lastName = "Cornelius";
+var age = 36;
 
 // Exercise 2
+let fullName = firstName + "" + lastName;
+console.log(fullName);
+fullName = `${firstName} ${lastName}`;
 
 // Exercise 3
+let city = "Alabaster";
+let passTime = "COD";
+let myStory = `Hi! My name is ${fullName}. I live in ${city}. i enjoy ${passTime} and coding!`;
+console.log(myStory);
